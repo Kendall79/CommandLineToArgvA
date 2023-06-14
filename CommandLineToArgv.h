3503baf0cd007265ac9af103b330b1cf07fd085e
Poppy <gkendall@qq.com>
2023-06-14 15:48:03 +0800

    Create CommandLineToArgv.h

---
+++ CommandLineToArgv.h
@@ -0,0 +1,22 @@
+#ifndef WINDOWS32_PLATFORM_COMMAND_LINE_TO_ARGV_H
+#define WINDOWS32_PLATFORM_COMMAND_LINE_TO_ARGV_H
+
+#ifdef __cplusplus
+extern "C"
+char ** CommandLineToArgvA_ola(char * commandLine, int *argc);
+#else
+extern
+char ** CommandLineToArgvA_ola(char * commandLine, int *argc);
+#endif // __cplusplus
+
+/* #define CommandLineToArgv macro */
+#if defined(UNICODE)|| defined(_UNICODE)
+/* inlude <Windows.h> */
+#define CommandLineToArgv CommandLineToArgvW
+#else
+#define CommandLineToArgv CommandLineToArgvA_ola
+#endif //defined(UNICODE)|| defined(_UNICODE)
+
+
+#endif // WINDOWS32_PLATFORM_COMMAND_LINE_TO_ARGV_H
+
